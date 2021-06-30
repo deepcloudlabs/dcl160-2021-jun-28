a4bd0c3b407584828952727e70944c2c28f901f1
deepcloudlabs <deepcloudlabs@gmail.com>
2021-06-30 15:42:37 +0300

    initial commit

--- module08-oop/exercise01.py
+++ module08-oop/exercise01.py
@@ -7,6 +7,9 @@ class InsufficientBalanceException(Exception):
             self.message = None
             self.deficit = 0.0
 
+    def __str__(self):
+        return f"InsufficientBalanceException( reason: {self.message}, deficit: {self.deficit})"
+
 
 class Account:
     # constructor -> python
@@ -25,21 +28,37 @@ class Account:
             raise ValueError(f"{amount} must be positive!")
         self.balance += amount
 
-    def withdraw(self, amount=10):
+    def withdraw(self, amount=10, withdrawAvailable=False):
+        """
+        Withdraws money
+        :param amount: withdraw amount
+        :return: withdrawn amount
+        """
         if amount <= 0:  # validation
             raise ValueError(f"{amount} must be positive!")
         if amount > self.balance:
-            message = f"Your balance ({self.balance}) does not cover your expenses({amount})"
-            deficit = amount - self.balance
-            raise InsufficientBalanceException(message, deficit)
-        self.balance -= amount
+            if withdrawAvailable:
+                available = self.balance
+                self.balance = 0
+                return available
+            else:
+                message = f"Your balance ({self.balance}) does not cover your expenses({amount})"
+                deficit = amount - self.balance
+                raise InsufficientBalanceException(message, deficit)
+        else:
+            self.balance -= amount
+            return amount
+
+    def __str__(self):
+        return f"Account( 'iban': {self.iban}, 'balance': {self.balance})"
 
 
 try:
     acc1 = Account(iban="tr1", balance=1_000_000)
-    acc1.withdraw(75_000)
-    acc1.withdraw(125_000)
+    print(acc1.withdraw(75_000))
+    print(acc1.withdraw(amount=1_250_000, withdrawAvailable=True))
     print(f"{acc1.iban}: {acc1.balance}")
+    print(acc1)  # Account::__str__
 except ValueError as err:
     print(err)
 except InsufficientBalanceException as err:
