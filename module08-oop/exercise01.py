2bbf6e2515164768a5123eea54d3cb068f69623c
deepcloudlabs <deepcloudlabs@gmail.com>
2021-07-01 10:10:46 +0300

    initial commit

--- module08-oop/exercise01.py
+++ module08-oop/exercise01.py
@@ -1,3 +1,6 @@
+from typing import Type
+
+
 class InsufficientBalanceException(Exception):
     def __init__(self, *args):
         if args:
@@ -11,7 +14,7 @@ class InsufficientBalanceException(Exception):
         return f"InsufficientBalanceException( reason: {self.message}, deficit: {self.deficit})"
 
 
-class Account:
+class Account(object):
     # constructor -> python
     def __init__(self, iban, balance=0.0):
         """
@@ -29,12 +32,17 @@ class Account:
         print("Account's destructor is working...")
         pass
 
+    def __add__(self, amount): # operator +
+        if isinstance(amount, float):
+            self.balance += amount
+
     def deposit(self, amount=10):
         if amount <= 0:  # validation
             raise ValueError(f"{amount} must be positive!")
         self.balance += amount
 
     def withdraw(self, amount=10, withdrawAvailable=False):
+        print("Account::withdraw")
         """
         Withdraws money
         :param amount: withdraw amount
@@ -78,6 +86,7 @@ class CheckingAccount(Account):
         pass
 
     def withdraw(self, amount=10, withdrawAvailable=False):
+        print("CheckingAccount::withdraw")
         if amount <= 0:  # validation
             raise ValueError(f"{amount} must be positive!")
         elif amount > (self.balance + self.overdraft_amount):
@@ -97,13 +106,65 @@ class CheckingAccount(Account):
             self.balance -= amount
             return amount
 
+    def __str__(self):
+        return f"CheckingAccount( 'iban': {self.iban}, 'balance': {self.balance})"
+
+
+from functools import reduce
+
+
+class Customer:
+
+    def __init__(self, fullname, identity):
+        self.fullname = fullname
+        self.identity = identity
+        self.accounts = {}  # composition -> 1-to-many
+
+    def add_account(self, account):
+        self.accounts[account.iban] = account
+
+    def get_account(self, iban):
+        """
+            for acc in self.accounts:
+                if acc.iban == iban:
+                    return acc
+            return None
+        """
+        return self.accounts[iban] if iban in self.accounts else None
+
+
+import random
 
 try:
-    acc1 = Account(iban="tr1", balance=1_000_000)
+    acc1 = Account(iban="tr1", balance=1_000)
+    print(acc1)
+    acc1 + 1000.0
+    print(acc1)
+    if random.randint(1, 10) < 6:
+        acc1 = Account(iban="tr1", balance=1_000_000)
+    else:
+        acc1 = CheckingAccount(iban="TR3", balance=50_000, overdraft_amount=1500)
+
+    print(acc1)
+    acc1.withdraw(50_000)
+
+    accounts = [Account(iban="tr1", balance=1_000_000),
+                CheckingAccount(iban="TR3", balance=50_000, overdraft_amount=1500)]
+    print("for acc in accounts:")
+    for acc in accounts:
+        print(acc)
+        if isinstance(acc, CheckingAccount):
+            print("acc references to an object from CheckingAccount class")
+        elif isinstance(acc, Account):
+            print("acc references to an object from Account class")
+        else:
+            print("acc references to an object!")
+        acc.withdraw(100)
+
     # del acc1 # triggers the destructor
-    acc1 = CheckingAccount(iban="TR3")
+    acc1.withdraw(10_000)  # CheckingAccount -> withdraw
     acc2 = CheckingAccount(iban="TR2", balance=50_000, overdraft_amount=10_000)
-    print(acc1.withdraw(75_000))
+    print(acc1.withdraw(75_000))  # withdraw(acc1,75_000)
     print(acc1.withdraw(amount=1_250_000, withdrawAvailable=True))
     print(f"{acc1.iban}: {acc1.balance}")
     print(acc1)  # Account::__str__
